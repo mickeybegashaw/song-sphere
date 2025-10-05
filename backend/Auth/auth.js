6d876fd79983c5b37f91298ce6046dd651bf5bc5
Mikiyas Begashaw <mickeyjob2004@gmail.com>
2025-10-05 21:04:33 +0300

    Implement authentication with better-auth and MongoDB integration; update Docker and package configurations

---
+++ backend/Auth/auth.js
@@ -0,0 +1,32 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URI =process.env.MONGO_URI
+
+const client = new MongoClient( MONGO_URI);
+const db = client.db();
+
+export const auth = betterAuth({
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID , 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET , 
+        }, 
+    },
+  database: mongodbAdapter(db, {
+    client
+  }),
+  trustedOrigins: [
+    process.env.FRONTEND_URL || "http://localhost:5173"],
+  
+
+  session: {
+    maxAge: 7 * 24 * 60 * 60, 
+    updateAge: 24 * 60 * 60,  
+  },
+  baseURL: process.env.BACKEND_URL || "http://localhost:5000",
+  
+});
